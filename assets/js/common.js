0f4ab4662582da661a65dd59e207a6c2dc873480
siruzhong <1412578784@qq.com>
2026-09-09 13:15:49 +0800

    update

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,14 +1,16 @@
-// aHR0cHM6Ly9naXRodWIuY29tL2x1b3N0MjYvYWNhZGVtaWMtaG9tZXBhZ2U=
 $(function () {
     $('.lazy').Lazy({
         scrollDirection: 'vertical',
         effect: 'fadeIn',
         effectTime: 300,
         visibleOnly: true,
-        placeholder: "",
-        onError: function(element) {
-            console.log('[lazyload] Error loading ' + element.data('src'));
-        }
-    })
-    $('[data-toggle="tooltip"]').tooltip()
-})
+        placeholder: ''
+    });
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    // Collapse the mobile menu after navigating to an in-page section.
+    $('.navbar-nav .nav-link').on('click', function () {
+        $('.navbar-collapse').collapse('hide');
+    });
+});
